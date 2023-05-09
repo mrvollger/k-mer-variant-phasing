d6bb4ad53ebef27738c63bce2c2205bac5e8cb8d
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-05-08 18:13:23 -0700

    change model input

--- workflow/scripts/merge-kmer-and-variant-phasing.py
+++ workflow/scripts/merge-kmer-and-variant-phasing.py
@@ -118,7 +118,7 @@ def main():
         .groupby(["phase_block", "variant_hap"], group_keys=False)
         .apply(lambda row: assign_based_on_kmer(row, args.max_frac_disagree))
     )
-    logging.info(f"merged df\n{merged_df}")
+    logging.info(f"merged df\n{merged_df.head()}")
     # count disagreements
     has_kmer = merged_df.kmer_hap != UNKNOWN
     disagreements = merged_df[has_kmer].kmer_hap != merged_df[has_kmer].merged_hap
