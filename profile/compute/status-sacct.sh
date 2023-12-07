426de6ec1b4e29f1210b7ad017c1b1e4a30a3926
Mitchell R. Vollger <mrvollger@gmail.com>
2023-12-07 12:50:19 -0800

    adding test for python and snakemake versions

--- profile/compute/status-sacct.sh
+++ profile/compute/status-sacct.sh
@@ -4,21 +4,18 @@
 
 jobid="$1"
 
-if [[ "$jobid" == Submitted ]]
-then
-  echo smk-simple-slurm: Invalid job ID: "$jobid" >&2
-  echo smk-simple-slurm: Did you remember to add the flag --parsable to your sbatch call? >&2
-  exit 1
+if [[ "$jobid" == Submitted ]]; then
+    echo smk-simple-slurm: Invalid job ID: "$jobid" >&2
+    echo smk-simple-slurm: Did you remember to add the flag --parsable to your sbatch call? >&2
+    exit 1
 fi
 
-output=`sacct -j "$jobid" --format State --noheader | head -n 1 | awk '{print $1}'`
+output=$(sacct -j "$jobid" --format State --noheader | head -n 1 | awk '{print $1}')
 
-if [[ $output =~ ^(COMPLETED).* ]]
-then
-  echo success
-elif [[ $output =~ ^(RUNNING|PENDING|COMPLETING|CONFIGURING|SUSPENDED).* ]]
-then
-  echo running
+if [[ $output =~ ^(COMPLETED).* ]]; then
+    echo success
+elif [[ $output =~ ^(RUNNING|PENDING|COMPLETING|CONFIGURING|SUSPENDED).* ]]; then
+    echo running
 else
-  echo failed
+    echo failed
 fi
