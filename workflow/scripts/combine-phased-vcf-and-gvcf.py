02509833db528ccb5015bbe2b4127979af0d40a1
Mitchell Robert Vollger <mrvollger@gmail.com>
2024-06-25 11:54:39 -0700

    feat: phased gvcf

--- workflow/scripts/combine-phased-vcf-and-gvcf.py
+++ workflow/scripts/combine-phased-vcf-and-gvcf.py
@@ -44,7 +44,7 @@ def run(vcf: Path, gvcf: Path, outfile: Path, break_n=None):
     add_phasing_format_to_header(gvcf)
     o_gvcf = Writer(outfile, gvcf, mode="wz")
     o_gvcf.add_to_header(
-        f"{sys.argv[0]}##Command={}".format(" ".join(sys.argv)),
+        f"{sys.argv[0]}##Command={" ".join(sys.argv)}",
     )
     change_count = 0
     for idx, rec in enumerate(tqdm(gvcf)):
